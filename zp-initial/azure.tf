f1f3e6f0e63380fc7f583f7e58c84359cd8a3596
Piyush Jain <piyush.jain240591@gmail.com>
2022-01-27 14:35:48 +0800

    update azure.tf

--- zp-initial/azure.tf
+++ zp-initial/azure.tf
@@ -1,3 +1,20 @@
 provider "azurerm" {
-    
+    features {}
+    alias = "personal"
+    subscription_id = ""
+    tenant_id = ""
+    client_id = ""
+    client_secret = ""
+}
+
+provider "azurerm" {
+    features {}
+    alias = "palo"
+    subscription_id = ""
+    tenant_id = ""
+    client_id = ""
+    client_secret = ""
+}
+
+data "azurerm_client_config" "current" {
 }
